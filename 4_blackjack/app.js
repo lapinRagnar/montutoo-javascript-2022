27a01a835ea9e8313a0afc7e796bdf3e6487b11b
lapinRagnar <lapinragnar@gmail.com>
2022-01-29 23:46:11 +0100

    suite - jeu blackjack

--- 4_blackjack/app.js
+++ 4_blackjack/app.js
@@ -7,14 +7,39 @@
 firstCard = 6
 secondCard = 9
 sum = firstCard + secondCard
+hasBlackjack = false
+isAlive = true
+message = ""
+messageEl = document.getElementById('message-el')
+sumEl = document.querySelector('#sum-el')
+cardsEl = document.getElementById('cards-el')
 
-if ( sum < 21 ) {
-    console.log("est-ce que tu veux une autre carte?? 😇");
-} else if ( sum === 21 ){
-    console.log("wooooooow, blackjack mon pote! 😆 🍾");
-} else {
-    console.log(" tu as perdu! 🤮");
+// cardsEl.textContent = String(firstCard) + '  ' + String(secondCard)
+
+startGame = () => {
+
+    sumEl.textContent += sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+
+    if ( sum <= 20 ) {
+        message = "est-ce que tu veux une autre carte?? 😇"
+    } else if ( sum === 21 ){
+        message = "wooooooow, blackjack mon pote! 😆 🍾"
+        hasBlackjack = true
+    } else {
+        message = " tu as perdu! 🤮"
+        isAlive = false
+    }
+    
+    console.log(message);
+    messageEl.textContent = message
 }
+
+newCard = () => {
+    console.log("ok");
+}
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # montutoo-javascript-2022
 tuto javascript 2022
+
+
+### site du tuto
+https://www.youtube.com/watch?v=jS4aFq5-91M
+
