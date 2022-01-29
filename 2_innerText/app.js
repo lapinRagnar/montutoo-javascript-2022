e2a794c2d1200878daad2f1f17d88bd23a69b045
lapinRagnar <lapinragnar@gmail.com>
2022-01-29 17:42:43 +0100

    utilisation de innerText

---
+++ 2_innerText/app.js
@@ -0,0 +1,8 @@
+bonjourElement = document.getElementById("bonjourElement")
+
+let nom = "Claude "
+let bonjour = "salut bonjour ca va!"
+
+bonjourElement.innerText = bonjour + nom
+bonjourElement.innerText += "😆" 
+
