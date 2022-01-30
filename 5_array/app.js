655d831ce3e9eb1a897004fe5b619210959cc6c1
lapinRagnar <lapinragnar@gmail.com>
2022-01-30 22:16:54 +0100

    exo sur les array - utilisation des fonctions shift et unshift

--- 5_array/app.js
+++ 5_array/app.js
@@ -7,3 +7,38 @@ console.log(card);
 // suprimmer la derniere valeur du tableau
 card.pop()
 console.log(card);
+
+/* 
+La méthode shift() permet de retirer le premier 
+élément d'un tableau et de renvoyer cet élément.
+Cette méthode modifie la longueur du tableau.
+*/
+
+/* 
+unshift()
+La méthode unshift() ajoute un ou plusieurs éléments au début d'un 
+tableau et renvoie la nouvelle longueur du tableau.
+*/
+largeCountries = [
+    "Tuvalu",
+    "India",
+    "USA",
+    "Indonesia",
+    "Monaco"
+]
+
+
+console.log(largeCountries);
+// premierElement = largeCountries.shift()
+// console.log("le 1er element = " + premierElement);
+// console.log(largeCountries);
+
+// remplacer Monaco par Pakistant
+largeCountries.pop()
+largeCountries.push("Pakistan")
+console.log(largeCountries);
+
+// remplacer Tuvalu en China
+largeCountries.shift()
+largeCountries.unshift("China")
+console.log(largeCountries);
