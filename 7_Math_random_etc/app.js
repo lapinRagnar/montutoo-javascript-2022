e26f6614fb0a9c7284846309a6109136807c6f06
lapinRagnar <lapinragnar@gmail.com>
2022-01-30 19:19:51 +0100

    Math-nombre aléatoire - lancé de dé

--- 7_Math_random_etc/app.js
+++ 7_Math_random_etc/app.js
@@ -4,3 +4,16 @@ Math.random() * 6  ===> valeur aleatoire entre 0 et 5
 Math.floor() ==> enlever les virgules
 
 */
+
+
+// dé 6 faces ....
+// generer un nombre aléatoire entre 1 et 6
+rollDice = ()=>{
+    let randomNumber = Math.floor(Math.random() * 6) + 1
+    return randomNumber
+}
+
+
+
+nombre = rollDice()
+console.log(nombre);
