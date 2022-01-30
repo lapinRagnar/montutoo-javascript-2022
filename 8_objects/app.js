4bcc9d9dd68a7f05a7aa6e0511169855678e9f33
lapinRagnar <lapinragnar@gmail.com>
2022-01-30 21:51:30 +0100

    exercice pratique - object

--- 8_objects/app.js
+++ 8_objects/app.js
@@ -16,3 +16,26 @@ console.log(course.length)
 console.log(course.level)
 console.log(course.tags)
 console.log(course['tags'])
+
+/**
+ *  - create a person object that contains keys: name, age, country
+ *  - use yourself as an example to set the values for name, age, country
+ * 
+ *  - create a function, logData(), that uses the person object to create
+ *      a string in the following format:
+ *      "Rabe is 35 years old and lives in Norway"
+ * 
+ *  - call the logData() function to verify that it works
+ */
+
+person = {
+    name: "Bitro",
+    age: 15,
+    country: "france"
+}
+
+logData = ()=>{
+    console.log(person.name + " a " + person.age + " ans et vit en " + person.country);
+}
+
+logData()
