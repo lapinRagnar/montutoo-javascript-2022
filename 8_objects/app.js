35ac1b5403123231d43ecbf340948ed6d97d5f83
lapinRagnar <lapinragnar@gmail.com>
2022-01-30 20:15:55 +0100

    introduction - programmation orienté objet

---
+++ 8_objects/app.js
@@ -0,0 +1,18 @@
+// Introduction object
+
+course = {
+    title: 'Apprendre css gratuitement',
+    lessons: 16,
+    creator: "Monsieur lapin",
+    length: 62,
+    level: 2,
+    isFree: true,
+    tags: ['html', 'css', 'js']
+
+}
+
+// pour acceder aux données
+console.log(course.length)
+console.log(course.level)
+console.log(course.tags)
+console.log(course['tags'])
