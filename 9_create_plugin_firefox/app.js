19295edbae6f85bac17f8bd79ad1b3d2806051fb
lapinRagnar <lapinragnar@gmail.com>
2022-02-02 20:56:54 +0100

    saisir sur une input et affichier dans une liste html via js

--- 9_create_plugin_firefox/app.js
+++ 9_create_plugin_firefox/app.js
@@ -1,22 +1,26 @@
-myLeads = ["a", "b", "c"]
+myLeads = []
 inputEl = document.getElementById('input-el')
 inputBtn = document.getElementById('input-btn')
 
 ulEl = document.getElementById('ul-el')
 
-
 inputBtn.addEventListener('click', () => {
     myLeads.push(inputEl.value)
-
+    renderLeads()
+    inputEl.value = ""
 })
 
+renderLeads = ()=>{
 
-listItems = ""
-for (i=0; i<myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li>"
-    console.log(listItems);
+    listItems = ""
+    for (i=0; i<myLeads.length; i++){
+        listItems += "<li>" + myLeads[i] + "</li>"
+        console.log(listItems);
+    }
+
+    ulEl.innerHTML = listItems
 }
 
-ulEl.innerHTML = listItems
+
 
 
