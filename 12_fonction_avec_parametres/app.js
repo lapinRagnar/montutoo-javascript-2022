9382fa8ba2ac784cb006d7c4516cec64bc2d7a3f
lapinRagnar <lapinragnar@gmail.com>
2022-02-04 18:49:00 +0100

    fonction - difference entre parametres et arguments

--- 12_fonction_avec_parametres/app.js
+++ 12_fonction_avec_parametres/app.js
@@ -1,10 +1,17 @@
+// difference entre parametre et argument
+
 welcomeEl = document.getElementById('welcome-el')
 
+// parametre veut dire dans la fonction
 function greetUser(salutation, name, emoji){
     welcomeEl.textContent = `Re ${salutation}, monsieur le developpeur ${name} ${emoji} `
 }
 
-greetUser("bonjour", "bitro de bitro b", '👏')
+
+// argument veut dire - à l'exterieur de la fonction
+// ex: hi, "bitro de bitro b", '👏' sont des arguments
+hi = 'bonjour'
+greetUser(hi, "bitro de bitro b", '👏')
 
 
 // fonction qui ajoute 2 nombres et retourne la somme
