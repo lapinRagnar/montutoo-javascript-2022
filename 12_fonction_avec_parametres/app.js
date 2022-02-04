44512bbacf816b2962647380a0f0f39c22795b3b
lapinRagnar <lapinragnar@gmail.com>
2022-02-04 18:27:43 +0100

    tuto - fonction avec parametre

---
+++ 12_fonction_avec_parametres/app.js
@@ -0,0 +1,9 @@
+welcomeEl = document.getElementById('welcome-el')
+
+function greetUser(name){
+    welcomeEl.textContent = 'Re bonjour, monsieur le developpeur '+ name + ' 👏'
+}
+
+greetUser("bitro de bitro b")
+
+
