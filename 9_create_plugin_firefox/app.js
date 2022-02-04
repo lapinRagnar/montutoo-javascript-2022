2661edfecfad2cdf3f6f0486aa87e2b29fac47d6
lapinRagnar <lapinragnar@gmail.com>
2022-02-04 18:15:43 +0100

    refactoring - utiliser fonction avec parametre

--- 9_create_plugin_firefox/app.js
+++ 9_create_plugin_firefox/app.js
@@ -1,34 +1,70 @@
 myLeads = []
+oldLeads = []
 inputEl = document.getElementById('input-el')
 inputBtn = document.getElementById('input-btn')
 ulEl = document.getElementById('ul-el')
+deleteBtn = document.getElementById('delete-btn')
 
 
 leadsFromLocalStorage = JSON.parse((localStorage.getItem('myLeads')))
-console.log(leadsFromLocalStorage);
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+} 
 
-inputBtn.addEventListener('click', () => {
-    myLeads.push(inputEl.value)
-    renderLeads()
-    inputEl.value = ""
-    localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    console.log(localStorage.getItem('myLeads'));
-})
+/* 
+    - rendre la foncttion renderLeads() plus dynamique
+    - donner une parametre a la fonction renderLeads()  comme ca, renderLeads(myLeads)
+    - changer la fonction renderLeads() en render()
 
-renderLeads = ()=>{
+*/
+// function renderLeads(){
+
+//     listItems = ""
+//     for (i=0; i<myLeads.length; i++){
+//         // listItems += "<li> <a href='' target='_blank'>" + myLeads[i] + " </a> </li>"
+//         listItems += `
+//             <li> 
+//                 <a href='#' target='_blank'> ${myLeads[i]}  </a> 
+//             </li>
+//         `
+//     }
+
+//     ulEl.innerHTML = listItems
+// }
+
+function render(leads){
 
     listItems = ""
-    for (i=0; i<myLeads.length; i++){
+    for (i=0; i<leads.length; i++){
         // listItems += "<li> <a href='' target='_blank'>" + myLeads[i] + " </a> </li>"
         listItems += `
             <li> 
-                <a href='#' target='_blank'> ${myLeads[i]}  </a> 
+                <a href='#' target='_blank'> ${leads[i]}  </a> 
             </li>
         `
     }
 
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener('dblclick', ()=>{
+    console.log('marche');
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+    location.reload()
+})
+
+inputBtn.addEventListener('click', () => {
+    myLeads.push(inputEl.value)
+    render(myLeads)
+    inputEl.value = ""
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    console.log(localStorage.getItem('myLeads'));
+})
+
+
 
 
 
